37d943406edd7c4bcffcf6eed66af0a2cfea322f
divyasaireddy <divyasaireddy0910@gmail.com>
2024-10-08 22:28:16 +0530

    added all files

---
+++ fun .js
@@ -0,0 +1,32 @@
+console.log("Question-1");
+function add(a,b){
+    console.log(a+b);
+}
+add(10,20);
+
+console.log("Question-2");
+function subtract(a,b){
+    console.log(a-b);
+}
+subtract(20,10)
+
+console.log("Question-3");
+function multiply(a,b){
+    console.log(a*b);
+}
+multiply(20,10)
+
+
+console.log("Question-4");
+function divide(a,b){
+    if(b==0){
+        console.log("cannot divide by zero");
+    }else{
+    console.log(a/b);
+    }
+}
+divide(20,0)
+divide(40,20)
+
+
+
---
+++ practice.js
@@ -0,0 +1,32 @@
+// var userInput=prompt("enter");
+// var day="tuesday";
+// switch(userInput){
+//     case "monday":
+//     console.log("It is monday");
+//     break;
+//     case "tuesday":
+//     console.log("It is tuesday");
+//     break;
+//     case "wednesday":
+//     console.log("It is wednesday");
+//     break;
+//     case "thursady":
+//     console.log("It is thursday");
+//     break;
+//     case "Friday":
+//     console.log("It is friday");
+//     break;
+//     default:
+//     console.log("It is weekend");
+//     }
+
+
+
+    
+
+let arr=[1,2,3,4,10,11];
+let res=[]
+for(i=0;i<=arr.length-1;i++){
+    arr.push(arr[i])
+}
+console.log(arr[i]);
